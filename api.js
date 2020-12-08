ce9c3438b6f2d1eb863610ecc95a53dfdc842181
igorbrands <igorcantelmo@gmail.com>
2020-12-07 22:42:36 -0300

    ajax and reduce total cart done

--- api.js
+++ api.js
@@ -1,5 +1,23 @@
 const api = (config, callback) => {
+  var xhr = new XMLHttpRequest();
 
-}
+  xhr.open(config.method, config.url);
+  xhr.send(config.method);
 
-export default api
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status == 200) {
+      const data = JSON.parse(xhr.responseText);
+
+      callback(data);
+    }
+  };
+};
+
+export default api;
+
+// solução com fetch():
+// fetch('./products.json')
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+//     .catch((error) => console(error));
+// };
--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-import api from './api.js'
+import api from './api.js';
 
+//Função que lista produtos e total no html:
+function listProducts({ item }) {
+  const $productsList = document.querySelector('.product-list');
+  const $productsTotal = document.querySelector('.product-total');
+
+  item.forEach(({ name, image, bestPriceFormated, quantity }) => {
+    const products = document.createElement('li');
+    products.innerHTML = `
+        <img src="${image}" />
+        <p>${name}</p>
+        <p>Qtd.: ${quantity}</p>
+        <p>${bestPriceFormated}</p>
+    `;
+
+    $productsList.appendChild(products);
+  });
+
+  const totalCart = item.reduce(
+    (acc, { bestPrice, quantity }) => (acc + (bestPrice * quantity)/100),
+    0
+  );
+
+  const formatedTotal = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL', minimumFractionDigits: 2 }).format(totalCart)
+
+
+  $productsTotal.innerHTML = `
+    Total do pedido: <span>${formatedTotal}</span>
+  `;
+}
+
+//parâmetros da requisação AJAX:
+const config = {
+  method: 'GET',
+  url: './products.json',
+  type: null,
+};
+
+api(config, listProducts);
 
 //Neste exercício você deve imprimir na UL ".product-list" produtos seguindo o layout no README do gitbub: https://i.imgur.com/EbVlWpX.png
 //Deve incluir a soma dos produtos
